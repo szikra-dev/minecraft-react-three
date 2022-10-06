6b07c93b39c594a9d9179dab22893619a5243296
David Arcís Moreno <szikra.dev@gmail.com>
2022-10-06 02:56:54 +0200

    adds useStore hook and a default cube in the store and cube method to add

---
+++ src/hooks/useStore.js
@@ -0,0 +1,29 @@
+import { nanoid } from 'nanoid'
+import create from 'zustand'
+
+export const useStore = create((set) => ({
+  texture: 'dirt',
+  cubes: [
+    {
+      key: nanoid(),
+      position: [1, 1, 1],
+      texture: 'dirt',
+    },
+  ],
+  addCube: (x, y, z) => {
+    set((prev) => ({
+      cubes: [
+        ...prev.cubes,
+        {
+          key: nanoid(),
+          position: [x, y, z],
+          texture: prev.texture,
+        },
+      ],
+    }))
+  },
+  removeCube: () => {},
+  setTexture: () => {},
+  saveWorld: () => {},
+  resetWorld: () => {},
+}))
